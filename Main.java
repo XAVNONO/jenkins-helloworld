8a38100e12137804265203a53d99c881db96e360
NOWACKI XAVIER <118280294+XAVNONO@users.noreply.github.com>
2023-08-22 15:26:33 +0200

    Update Main.java
    Ajout message pour trigger

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Xavier!");
+        System.out.println("Hello, Xavier! vérification du trigger");
     }
 }
 
